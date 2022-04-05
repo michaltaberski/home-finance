c9b67e8b641881055fd0247c5bcb0cc64530cf0b
Michał Taberski <m.taberski@gmail.com>
2022-04-05 11:34:02 +0200

    wip

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "@todcode/components";
+import {} from "@home-finance/shared";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  console.log(Button);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         Your count is {count}
-        <Button onClick={() => setCount((prev) => ++prev)} />
+        <button onClick={() => setCount((prev) => ++prev)} />
       </header>
     </div>
   );
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,3 @@
+export enum DemoEnum {
+  VAL_1 = "I ma the val1",
+}
